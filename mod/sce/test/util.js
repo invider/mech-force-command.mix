547cdeff5b832e551a1f6da464db99901da44794
Igor Khotin <khotin@gmx.com>
2021-03-13 20:50:02 +0200

    [+] more sfx + license info on sfx

--- mod/sce/test/util.js
+++ mod/sce/test/util.js
@@ -1,6 +1,7 @@
 const msg = 'ALL TESTS PASSED!'
 
 function allTestsPassed() {
+    if (env.config.test !== true) return
     log(msg)
     lab.mode.statusBar.status = msg
     lib.sfx('testsPassed')
