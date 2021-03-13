a79635e91a9ebb2ef017ab1c91cb4d9d220fb4e3
Igor Khotin <khotin@gmx.com>
2021-03-13 22:37:24 +0200

    [+] stat on title bar

--- mod/dna/hud/TitleBar.js
+++ mod/dna/hud/TitleBar.js
@@ -22,6 +22,19 @@ class TitleBar extends dna.hud.Panel {
         if (lab.world) lab.world.hide()
     }
 
+    drawTeamStat(team, tstat, x, y, w) {
+        let line
+        if (w < 10) {
+            line = team.name.toUpperCase().substring(0, 1) + ':' + tstat.units
+        } else {
+            line = team.name.toUpperCase() + ':' + tstat.units
+        }
+        this.__
+            .face( team.color() )
+            .at(x, y)
+            .print(line)
+    }
+
     draw() {
         const world = this.world
         const hero = world.hero
@@ -32,23 +45,36 @@ class TitleBar extends dna.hud.Panel {
         tx
             .reset()
             .at(0, 0)
-            .back(lib.cidx('baseHi'))
+            .back(lib.cidx('baseLow'))
             .face(lib.cidx('alert'))
 
         for (let x = 0; x < w; x++) {
             tx.out(' ')
         }
 
+        const stat = lab.control.stat.teamStat()
         if (this.title) {
             tx
                 .face(lib.cidx('alert'))
                 .at(0, 0)
                 .print(this.title)
+        } else if (stat) {
+            const teamW = floor((w - 10)/5)
+
+            let x = 0
+            const y = 0
+            for (let i = 1; i < env.team.length; i++) {
+                this.drawTeamStat(env.team[i], stat[i], x, y, teamW)
+                x += teamW
+            }
+            this.drawTeamStat(env.team[0], stat[0], x, y, teamW)
         }
 
         let turn = 'Turn:' + world.turn
         if (world.scheduled) turn += '<' + world.scheduled
-        tx.at(w - 10, 0).print(turn)
+        tx
+            .face(lib.cidx('alert'))
+            .at(w - 10, 0).print(turn)
 
         /*
         tx.at(1, 0).print('' + this.world.hero.x + ':'
