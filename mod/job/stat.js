37d143af14a59d6fa62f6501aaec3a70d065f442
Igor Khotin <khotin@gmx.com>
2021-03-14 12:23:05 +0200

    [+] player stat

--- mod/job/stat.js
+++ mod/job/stat.js
@@ -3,17 +3,25 @@ function start() {
         const units = env.team[i].unitsAlive()
         this.state.team[i].units = units
         this.state.team[i].original = units
-        this.state.team[i].captured = 0
     }
 }
 
 function captureCase(captured, context) {
     this.state.team[context.team].captured ++
+    if (context.source.taken) {
+        this.state.team[context.team].pcaptured ++
+    }
 }
 
 function killCase(killed, context) {
     this.state.team[context.source.team].kills ++
+    if (context.source.taken) {
+        this.state.team[context.source.team].pkills ++
+    }
     this.state.team[killed.team].destroyed ++
+    if (killed.taken) {
+        this.state.team[killed.team].pdestroyed ++
+    }
 }
 
 function register(event, source, context) {
@@ -49,8 +57,12 @@ function reset() {
         this.state.team[i] = {
             units:      0,
             original:   0,
+            captured:   0,
             kills:      0,
             destroyed:  0,
+            pcaptured:  0,
+            pkills:     0,
+            pdestroyed: 0,
         }
     }
 }
